b2af49deccc1925d5b01daeea66d72fbca261806
Smitha Milli <smitha.milli@gmail.com>
2014-01-04 21:56:09 -0600

    Draw question & answer cards on user connection

--- public/javascripts/game-ui.js
+++ public/javascripts/game-ui.js
@@ -11,15 +11,31 @@ var socket = io.connect();
 $(document).ready(function() {
   var game;
 
+  // after server checks to make sure this is a valid room
   socket.on('init', function(data){
       game = new Game(socket, data.name, data.roomId);
       console.log('initialized')
   });
 
+  // the original hand that is dealt to user
+  socket.on('hand', function(cards){
+    for (var i = 0; i < cards.length; i++){
+      $('#game').append($('<div></div>', {"class": "card"}).html(cards[i]));
+    }
+  });
+
+  // update the current question
+  socket.on('current question', function(question){
+    console.log(question);
+    $('#questionCard').html(question.text);
+  });
+
+  // receiving messages
   socket.on('message', function(message){
     $('#chat').append(divEscapedContentElement(message));
-  })
+  });
 
+  // submitting messages
   $('#message-form').submit(function(e){
     e.preventDefault();
     var message = $('#message').val();
@@ -30,4 +46,5 @@ $(document).ready(function() {
       $('#message').val('');
     }
   });
+
 });
